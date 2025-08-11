1453f84881c815a6918cd1162195c99611b04c64
petraalpigiano <petraalpigiano@gmail.com>
2025-08-11 13:04:30 +0200

    add index route

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        return view('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+
+Route::get('/', [PageController::class, 'index'])->name("home");
 
 Route::get('/contact', function () {
     return view('contact');
