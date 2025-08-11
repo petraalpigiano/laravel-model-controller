310d6fec9f793b84f317eaa59d76a556df7a3a94
petraalpigiano <petraalpigiano@gmail.com>
2025-08-11 13:36:34 +0200

    fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,9 +14,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href={{ route('contact') }}>Contact</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
+
             </ul>
         </div>
     </div>
