9e22c2e4a6aa224c3407bbe00b3f66b00a86ebc1
petraalpigiano <petraalpigiano@gmail.com>
2025-08-11 13:15:12 +0200

    add movies from db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+        // dd($movies);
+        return view('home', compact('movies'));
     }
 }
